a148be320b8fa24e70fdef9556e1419c442a006a
pb1646a <pb1646a@gmail.com>
2019-02-16 18:13:34 -0500

    error interceptor and confirm password validator

--- frontend/src/app/login/validators/password-validators.ts
+++ frontend/src/app/login/validators/password-validators.ts
@@ -1,9 +1,24 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, FormGroup } from "@angular/forms";
 export class PasswordValidator {
   static cannotContainSpace(formControl: FormControl) {
-    if (formControl.value.indexOf(" ") >= 0){
+    if (formControl.value.indexOf(" ") >= 0) {
       return { cannotContainSpace: true };
     }
     return null;
+  }
+  static mustMatchGroup(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatchGroup) {
+        return;
+      }
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatchGroup: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
   }
 }
